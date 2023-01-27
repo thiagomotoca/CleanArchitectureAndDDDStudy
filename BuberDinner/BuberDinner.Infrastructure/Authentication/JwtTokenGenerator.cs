b349455d302bbb8d2c78828c3e7af64cad484102
Thiago Cruz <thiago.cruz@mployeradvisor.com>
2023-01-27 09:40:47 -0300

    User repository

--- BuberDinner/BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ BuberDinner/BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -5,6 +5,7 @@ using BuberDinner.Application.Common.Interfaces.Authentication;
 using Microsoft.IdentityModel.Tokens;
 using BuberDinner.Application.Common.Interfaces.Services;
 using Microsoft.Extensions.Options;
+using BuberDinner.Domain.Entities;
 
 namespace BuberDinner.Infrastructure.Authentication;
 
@@ -21,7 +22,7 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         _jwtSettings = jwtOptions.Value;
     }
 
-    public string GenerateToken(Guid userId, string firstName, string lastName)
+    public string GenerateToken(User user)
     {
         var signingCredentials = new SigningCredentials(
             new SymmetricSecurityKey(
@@ -32,9 +33,9 @@ public class JwtTokenGenerator : IJwtTokenGenerator
 
         var claims = new[]
         {
-            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
-            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
-            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
         };
 
